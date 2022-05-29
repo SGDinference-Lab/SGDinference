4fd1ed4760c41cf33cbf3c29d15e1a141f4904f9
yshin12 <yshin12@gmail.com>
2022-05-29 19:20:24 -0400

    sgd_qr without inference

--- R/sgd_qr.R
+++ R/sgd_qr.R
@@ -32,50 +32,67 @@
 #' sgdi.out = sgdi(x,y)
 
 
-sgdi = function(x, y, z=NA, gamma_0=1, alpha=0.667, burn=1, model="lm", inference="rs",
-                     bt_start = NULL, path_output = NULL, Pi_s=NULL, qt=0.5,
-                     studentize = TRUE, intercept = TRUE
-){
-  #----------------------------------------------
-  # Linear (Mean) Regression 
-  #----------------------------------------------
-  if (model=="lm"){
-    test = sgdi_lm(x, y, gamma_0, alpha, burn, inference="rs",
-                   bt_start, path_output, 
-                   studentize, intercept
-    )
-    beta_hat = test$beta_hat
-    V_hat = test$V_hat
+sgd_qr = function(x, y, gamma_0=1, alpha=0.667, burn=1, 
+                bt_start = NULL, path_output = NULL, qt=0.5,
+                studentize = TRUE, intercept = TRUE
+                ){
+  x = as.matrix(x)
+  
+  if (studentize){
+    # Compute column means and standard errors and save them for later reconversion
+    x_mean = apply(x, 2, mean)
+    x_sd = apply(x, 2, sd)
+
+    # Studentize each column if x
+    x = apply(x, 2, function(.) (.-mean(.))/sd(.) )
   }
-  
-  
+
+  # Attach a vector of 1's for an intercept term
+  if (intercept){
+    x = cbind(1, x)
+  }
+
+  # Get the dimension of x and the sample size: p and n
+  p = ncol(as.matrix(x))
+  n = length(y)
+
+  # Initialize the bt_t, A_t, b_t, c_t
+  if (is.null(bt_start)){
+    bt_t = bar_bt_t = bt_start = matrix(0, nrow=p, ncol=1)
+  } else {
+    bt_t = bar_bt_t = matrix(bt_start, nrow=p, ncol=1)
+  }
+
+  n_path = length(path_output)
+  cnt_path = 1
+  beta_hat_path = matrix(NA, p, n_path)
+
   #----------------------------------------------
   # Quantile Regression
   #----------------------------------------------
-  if (model=="qr"){
-    out = sgdi_qr(x, y, gamma_0, alpha, burn, inference,
-                  bt_start, path_output, qt,
-                  studentize, intercept
-    )
+
+    out = sgd_qr_cpp(x, y, burn, gamma_0, alpha, bt_start=bt_t, tau=qt)
     beta_hat = out$beta_hat
-    V_hat = out$V_hat
+
+  # Re-scale parameters to reflect the studentization
+  if (studentize){
+    rescale_matrix = diag(1/x_sd)
+    if (intercept){
+      # Redefine the rescale_matrix including the intercept term
+      rescale_matrix = rbind(c(1,-(x_mean/x_sd)), cbind(0, rescale_matrix))
+    }
+    # Re-scale the parameters
+    beta_hat = rescale_matrix %*% beta_hat
   }
-  
-  if (model=="z"){
-    out = sgdi_z(x, y, z, gamma_0, alpha, burn, inference,
-                  bt_start, path_output, qt,
-                  studentize, intercept
-    )
-    beta_hat = out$beta_hat
-    V_hat = out$V_hat
-  }
-  
+
+
+
   if ( is.null(path_output)) {
-    return(list(beta_hat=beta_hat, V_hat = V_hat))
+    return(list(beta_hat=beta_hat))
   } else {
-    return(list(beta_hat = beta_hat, V_hat = V_hat, beta_hat_path = beta_hat_path, V_hat_path = V_hat_path))
+    return(list(beta_hat = beta_hat, beta_hat_path = beta_hat_path))
   }
-  
+
 }
 
 
---
+++ src/sgd_qr.cpp
@@ -0,0 +1,38 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+List sgd_qr_cpp(const arma::mat& x, const arma::colvec& y, const int& burn, const double& gamma_0, const double& alpha,
+             const arma::colvec& bt_start, const double& tau){
+  int n = y.n_elem;
+  double learning_rate_new;
+  arma::colvec gradient_bt_new;
+  arma::colvec bt_t = bt_start;
+  int p = bt_t.n_elem;
+  arma::colvec bar_bt_t;
+  bar_bt_t.zeros(p);
+
+  if (burn > 1) {
+    for(int obs = 1; obs < burn; obs++){
+      learning_rate_new = gamma_0 * std::pow(obs, -alpha);
+      gradient_bt_new = ( trans(x.row(obs-1)) * ( (y(obs-1) < as_scalar(x.row(obs-1) * bt_t)) - tau) );
+      bt_t = bt_t - learning_rate_new * gradient_bt_new;
+    }
+  }
+
+
+
+  // for (int obs = burn; obs < (n+1); obs++){
+  for (int obs = burn; obs < (n+1); obs++){
+    learning_rate_new = gamma_0 * std::pow(obs, -alpha);
+    gradient_bt_new = ( trans(x.row(obs-1)) * ( (y(obs-1) < as_scalar(x.row(obs-1) * bt_t)) - tau) );
+    bt_t = bt_t - learning_rate_new * gradient_bt_new;
+    bar_bt_t = ( bar_bt_t*(obs - burn) + bt_t ) / (obs - burn + 1);
+  }
+
+
+  return List::create(Named("beta_hat") = bar_bt_t);
+}
+
+
