bc14c027d08af001bd214da54df601a198014384
yshin12 <yshin12@gmail.com>
2022-09-17 11:27:20 -0400

    delete todo list

--- R/sgdi_lm.R
+++ R/sgdi_lm.R
@@ -31,10 +31,6 @@
 #' y = cbind(1,x) %*% bt0 + rnorm(n)
 #' out = sgdi_lm(x,y)
 
-# Todo list
-# (1) "rss" subset inference for linear regression
-# (2) path_output
-
 
 sgdi_lm = function(x, y, gamma_0=1, alpha=0.667, burn=1, inference="rs",
                 bt_start = NULL, path_output = NULL, 
