b8d7d1de56acf2d326b2dcc9429b5f5836e37583
yshin12 <yshin12@gmail.com>
2022-12-08 11:33:57 -0500

    codecov sgdi

---
+++ tests/testthat/test-sgdi.R
@@ -0,0 +1,12 @@
+test_that("sgdi vs sgdi_qr", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  out1 = sgdi(x,y, model="qr")
+  out2 = sgdi_qr(x,y)
+  check = max(abs(out1$beta_hat - out2$beta_hat))
+  expect_true(check==0)
+})
+
