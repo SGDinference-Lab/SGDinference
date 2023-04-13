c37dd9c5d121f1ea7cc212725ad02606cc43b8c8
Sokbae "Simon" Lee <sl3841@columbia.edu>
2023-04-13 19:35:18 -0400

    Description, vignette, etc

---
+++ R/SGDinference.R
@@ -0,0 +1,14 @@
+#' SGDinference
+#' 
+#' The SGDinference package provides estimation and inference methods for large-scale mean and quantile regression models via stochastic (sub-)gradient descent (S-subGD) algorithms. The inference procedure handles cross-sectional data sequentially: 
+#' (i) updating the parameter estimate with each incoming "new observation", 
+#' (ii) aggregating it as a Polyak-Ruppert average, and 
+#' (iii) computing an asympotically pivotal statistic for inference through random scaling.
+#' 
+#' @docType package
+#' @author Sokbae Lee, Yuan Liao, Myung Hwan Seo, Youngki Shin
+#' @import Rcpp stats
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib SGDinference
+#' @name SGDinference
+NULL
