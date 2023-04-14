153d99ead3dbcddf0cdfe4274ea9c2112e59a713
Sokbae "Simon" Lee <sl3841@columbia.edu>
2023-04-14 13:07:31 -0400

    Readme further updated

--- README.md
+++ README.md
@@ -1,15 +1,45 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
 # SGDinference
 
+<!-- badges: start -->
+
+[![R-CMD-check](https://github.com/SGDinference-Lab/SGDinference/workflows/R-CMD-check/badge.svg)](https://github.com/SGDinference-Lab/SGDinference/actions)
 [![codecov](https://codecov.io/gh/SGDinference-Lab/SGDinference/branch/master/graph/badge.svg?token=YTBY15IXEP)](https://codecov.io/gh/SGDinference-Lab/SGDinference)
+<!-- badges: end -->
 
-R package for the S-subGD method
+**SGDinference** is an R package that provides estimation and inference
+methods for large-scale mean and quantile regression models via
+stochastic (sub-)gradient descent (S-subGD) algorithms. The inference
+procedure handles cross-sectional data sequentially:
 
-Date: 2023-01-31
+1)  updating the parameter estimate with each incoming “new
+    observation”,
+2)  aggregating it as a Polyak-Ruppert average, and
+3)  computing an asympotically pivotal statistic for inference through
+    random scaling.
 
-- **sgd_lm** without inference.
+The methodology used in the SGDinference package is described in detail
+in the following papers:
 
-```r
+- Lee, S., Liao, Y., Seo, M.H. and Shin, Y., 2022. Fast and robust
+  online inference with stochastic gradient descent via random scaling.
+  In *Proceedings of the AAAI Conference on Artificial Intelligence*
+  (Vol. 36, No. 7, pp. 7381-7389).
+  <https://doi.org/10.1609/aaai.v36i7.20701>.
+
+- Lee, S., Liao, Y., Seo, M.H. and Shin, Y., 2023. Fast Inference for
+  Quantile Regression with Tens of Millions of Observations.
+  arXiv:2209.14502 \[econ.EM\]
+  <https://doi.org/10.48550/arXiv.2209.14502>.
+
 ## Installation
+
+You can install the development version from
+[GitHub](https://github.com/) with:
+
+``` r
 # install.packages("devtools") # if you have not installed "devtools" package
 devtools::install_github("SGDinference-Lab/SGDinference")
 ```
--- tests/testthat/test-sl.R
+++ tests/testthat/test-sl.R
@@ -35,7 +35,7 @@ test_that("sgdi_qr should work with option rsd", {
 ### Quantile Regression without Inference ###
 #############################################
 ## NB: Without inference, we do not choose any option on V.hat estimation.
-test_that("sgd_qr should work with option rs", {
+test_that("sgd_qr should work", {
   n = 1e05
   p = 5
   bt0 = rep(5,p)
@@ -45,26 +45,7 @@ test_that("sgd_qr should work with option rs", {
   out = sgd_qr(y~., data=my.dat)
   expect_error(print(out), NA)
 })
-test_that("sgd_qr should work with option rss", {
-  n = 1e05
-  p = 5
-  bt0 = rep(5,p)
-  x = matrix(rnorm(n*(p-1)), n, (p-1))
-  y = cbind(1,x) %*% bt0 + rnorm(n)
-  my.dat = data.frame(y=y, x=x)
-  out = sgd_qr(y~., data=my.dat)
-  expect_error(print(out), NA)
-})
-test_that("sgd_qr should work with option rsd", {
-  n = 1e05
-  p = 5
-  bt0 = rep(5,p)
-  x = matrix(rnorm(n*(p-1)), n, (p-1))
-  y = cbind(1,x) %*% bt0 + rnorm(n)
-  my.dat = data.frame(y=y, x=x)
-  out = sgd_qr(y~., data=my.dat)
-  expect_error(print(out), NA)
-})
+
 #######################
 ### Mean Regression ###
 #######################
@@ -101,7 +82,7 @@ test_that("sgdi_lm should work with option rsd", {
 #########################################
 ### Mean Regression without inference ###
 #########################################
-test_that("sgd_lm should work with option rs", {
+test_that("sgd_lm should work", {
   n = 1e05
   p = 5
   bt0 = rep(5,p)
@@ -111,24 +92,3 @@ test_that("sgd_lm should work with option rs", {
   out = sgd_lm(y~., data=my.dat)
   expect_error(print(out), NA)
 })
-test_that("sgd_lm should work with option rss", {
-  n = 1e05
-  p = 5
-  bt0 = rep(5,p)
-  x = matrix(rnorm(n*(p-1)), n, (p-1))
-  y = cbind(1,x) %*% bt0 + rnorm(n)
-  my.dat = data.frame(y=y, x=x)
-  out = sgd_lm(y~., data=my.dat)
-  expect_error(print(out), NA)
-})
-test_that("sgd_lm should work with option rsd", {
-  n = 1e05
-  p = 5
-  bt0 = rep(5,p)
-  x = matrix(rnorm(n*(p-1)), n, (p-1))
-  y = cbind(1,x) %*% bt0 + rnorm(n)
-  my.dat = data.frame(y=y, x=x)
-  out = sgd_lm(y~., data=my.dat)
-  expect_error(print(out), NA)
-})
-
