1dc3f3b09ba6546a3899e5515b864b44dbd97897
Sokbae "Simon" Lee <sl3841@columbia.edu>
2023-04-14 12:15:03 -0400

    test-sl.R and vignette updated

--- tests/testthat/test-sl.R
+++ tests/testthat/test-sl.R
@@ -1,6 +1,6 @@
-########################################################
-### There is an error message in quantile regression ###
-########################################################
+###########################
+### Quantile Regression ###
+###########################
 test_that("sgdi_qr should work with option rs", {
   n = 1e05
   p = 5
@@ -31,9 +31,42 @@ test_that("sgdi_qr should work with option rsd", {
   out = sgdi_qr(y~., data=my.dat, inference="rsd")
   expect_error(print(out), NA)
 })
-###############################################
-### There is no problem for mean regression ###
-###############################################
+#############################################
+### Quantile Regression without Inference ###
+#############################################
+test_that("sgd_qr should work with option rs", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out = sgd_qr(y~., data=my.dat, inference="rs")
+  expect_error(print(out), NA)
+})
+test_that("sgd_qr should work with option rss", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out = sgd_qr(y~., data=my.dat, inference="rss")
+  expect_error(print(out), NA)
+})
+test_that("sgd_qr should work with option rsd", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out = sgd_qr(y~., data=my.dat, inference="rsd")
+  expect_error(print(out), NA)
+})
+#######################
+### Mean Regression ###
+#######################
 test_that("sgdi_lm should work with option rs", {
   n = 1e05
   p = 5
@@ -62,6 +95,39 @@ test_that("sgdi_lm should work with option rsd", {
   y = cbind(1,x) %*% bt0 + rnorm(n)
   my.dat = data.frame(y=y, x=x)
   out = sgdi_lm(y~., data=my.dat, inference="rsd")
+  expect_error(print(out), NA)
+})
+#########################################
+### Mean Regression without inference ###
+#########################################
+test_that("sgd_lm should work with option rs", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out = sgd_lm(y~., data=my.dat, inference="rs")
+  expect_error(print(out), NA)
+})
+test_that("sgd_lm should work with option rss", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out = sgd_lm(y~., data=my.dat, inference="rss")
+  expect_error(print(out), NA)
+})
+test_that("sgd_lm should work with option rsd", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out = sgd_lm(y~., data=my.dat, inference="rsd")
   expect_error(print(out), NA)
 })
 
