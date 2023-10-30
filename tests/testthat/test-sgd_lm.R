be1105f8b622cf11715d1991545e58f440eccdca
Sokbae "Simon" Lee <sl3841@columbia.edu>
2023-10-30 19:18:15 -0400

    CXX_STD = CXX11 removed

--- tests/testthat/test-sgd_lm.R
+++ tests/testthat/test-sgd_lm.R
@@ -62,3 +62,30 @@ test_that("sgd_lm bt_start matters", {
   check = max(abs(out1$coefficients - out2$coefficients))
   expect_false(check==0)
 })
+
+test_that("sgd_lm burn matters 1", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = 2*matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgd_lm(y~., data=my.dat)
+  out2 = sgd_lm(y~., data=my.dat, burn=100)
+  check = max(abs(out1$coefficients - out2$coefficients))
+  expect_false(check==0)
+})
+
+test_that("sgd_lm burn matters 2", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = 2*matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgd_lm(y~., data=my.dat, studentize=FALSE)
+  out2 = sgd_lm(y~., data=my.dat, burn=100, studentize=FALSE)
+  check = max(abs(out1$coefficients - out2$coefficients))
+  expect_false(check==0)
+})
+
