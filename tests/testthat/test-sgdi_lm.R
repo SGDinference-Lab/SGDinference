344c915431b51a5a99d0513dc27398f3eae5ba6f
Sokbae "Simon" Lee <sl3841@columbia.edu>
2023-10-31 09:15:10 -0400

    Further tests added

--- tests/testthat/test-sgdi_lm.R
+++ tests/testthat/test-sgdi_lm.R
@@ -50,3 +50,42 @@ test_that("Run the code with an option studentize=F",
             expect_true(check<1e-2)
           }
 )
+
+test_that("sgdi_lm no_studentize matters", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = 2*matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgdi_lm(y~., data=my.dat)
+  out2 = sgdi_lm(y~., data=my.dat, no_studentize=1e06)
+  check = max(abs(out1$coefficients - out2$coefficients))
+  expect_false(check==0)
+})
+
+test_that("sgdi_lm with scalar X", {
+  n = 1e05
+  p = 2
+  bt0 = rep(5,2)
+  x = 2*matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgdi_lm(y~., data=my.dat, studentize=TRUE)
+  out2 = sgdi_lm(y~., data=my.dat, studentize=FALSE)
+  check = max(abs(out1$coefficients - out2$coefficients))
+  expect_false(check==0)
+})
+
+test_that("sgdi_lm with scalar X with/out intercept", {
+  n = 1e05
+  p = 2
+  bt0 = c(0,5)
+  x = 2*matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgdi_lm(y~., data=my.dat, intercept=TRUE)
+  out2 = sgdi_lm(y~., data=my.dat, intercept=FALSE)
+  check = max(abs(out1$coefficients[-1] - out2$coefficients))
+  expect_false(check==0)
+})
--- tests/testthat/test-sgdi_qr.R
+++ tests/testthat/test-sgdi_qr.R
@@ -65,3 +65,82 @@ test_that("Different significance levels",
             expect_true(check>0)
           }
 )
+
+test_that("sgdi_qr no_studentize matters", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = 2*matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgdi_qr(y~., data=my.dat)
+  out2 = sgdi_qr(y~., data=my.dat, no_studentize=1e06)
+  check = max(abs(out1$coefficients - out2$coefficients))
+  expect_false(check==0)
+})
+
+test_that("sgdi_qr with scalar X", {
+  n = 1e05
+  p = 2
+  bt0 = rep(5,2)
+  x = 2*matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgdi_qr(y~., data=my.dat, studentize=TRUE)
+  out2 = sgdi_qr(y~., data=my.dat, studentize=FALSE)
+  check = max(abs(out1$coefficients - out2$coefficients))
+  expect_false(check==0)
+})
+
+test_that("sgdi_qr with scalar X with/out intercept", {
+  n = 1e05
+  p = 2
+  bt0 = c(0,5)
+  x = 2*matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgdi_qr(y~., data=my.dat, intercept=TRUE)
+  out2 = sgdi_qr(y~., data=my.dat, intercept=FALSE)
+  check = max(abs(out1$coefficients[-1] - out2$coefficients))
+  expect_false(check==0)
+})
+
+test_that("sgdi_qr burn matters 1", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = 2*matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgdi_qr(y~., data=my.dat)
+  out2 = sgdi_qr(y~., data=my.dat, burn=100)
+  check = max(abs(out1$coefficients - out2$coefficients))
+  expect_false(check==0)
+})
+
+test_that("sgdi_qr burn matters 2", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = 2*matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgdi_qr(y~., data=my.dat, studentize=FALSE)
+  out2 = sgdi_qr(y~., data=my.dat, burn=100, studentize=FALSE)
+  check = max(abs(out1$coefficients - out2$coefficients))
+  expect_false(check==0)
+})
+
+
+test_that("sgdi_qr path_index matters", {
+  n = 1e05
+  p = 5
+  bt0 = rep(5,p)
+  x = matrix(rnorm(n*(p-1)), n, (p-1))
+  y = cbind(1,x) %*% bt0 + rnorm(n)
+  my.dat = data.frame(y=y, x=x)
+  out1 = sgdi_qr(y~., data=my.dat, path=TRUE, path_index=1)
+  out2 = sgdi_qr(y~., data=my.dat, path=TRUE, path_index=2)
+  check = max(abs(out1$beta_hat_path[100] - out2$beta_hat_path[100]))
+  expect_false(check==0)
+})
